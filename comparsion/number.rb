2f17e9babf0b4d3d34e22f1e9404f15cd314e1d7
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-05-28 17:30:14 +0700

    comparsion

---
+++ comparsion/number.rb
@@ -0,0 +1,8 @@
+puts 10 == 9 # false
+puts 10 == 10 # true
+puts 10 == '10' # false
+puts 10 == 10.0 # true
+puts 10 == 10.1 # false
+puts 10 != 9 # true
+puts 10 === 10.0 # true
+puts 10.eql? 10.0  # false karena membandingkan type
